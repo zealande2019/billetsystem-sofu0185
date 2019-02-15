ac0cf50ab73b4116f1830158628b699c0020640f
sofu0185 <sofu0185@edu.easj.dk>
2019-02-15 16:14:21 +0100

    commit 2

---
+++ BilletLibrary/BilletLibrary/Bil.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BilletLibrary
+{
+    /// <summary>
+    /// Repræsentere en basal bil i systemet
+    /// </summary>
+    public class Bil
+    {
+        /// <summary>
+        /// Bilens nummerplade
+        /// </summary>
+        public string Nummerplade { get; set; }
+        /// <summary>
+        /// Hvornår bilen kørte over broen
+        /// </summary>
+        public DateTime Dato { get; set; }
+
+        /// <summary>
+        /// Prisen for at krydse broen
+        /// </summary>
+        /// <returns></returns>
+        public decimal Pris() => 240;
+        /// <summary>
+        /// Retunere køretøjets type
+        /// </summary>
+        /// <returns></returns>
+        public string Køretøj() => "Bil";
+
+    }
+}
---
+++ BilletLibrary/UnitTestProject/UnitTestBil.cs
@@ -0,0 +1,39 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BilletLibrary;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTestBil
+    {
+        [TestMethod]
+        public void Pris_ReturVærdi_Retunere240()
+        {
+            //Arrange
+            Bil b = new Bil();
+            decimal expected = 240;
+            decimal actual = 0;
+
+            //Act
+            actual = b.Pris();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Køretøj_ReturVærdi_RetunereBil()
+        {
+            //Arrange
+            Bil b = new Bil();
+            string expected = "Bil";
+            string actual = "";
+
+            //Act
+            actual = b.Køretøj();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
