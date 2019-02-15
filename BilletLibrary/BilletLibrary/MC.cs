0b8958f6d04e75250741b70cd5610dd1f8a63c03
sofu0185 <sofu0185@edu.easj.dk>
2019-02-15 16:21:52 +0100

    commit 3

---
+++ BilletLibrary/BilletLibrary/MC.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BilletLibrary
+{
+    public class MC
+    {
+        /// <summary>
+        /// Motercyklens nummerplade
+        /// </summary>
+        public string Nummerplade { get; set; }
+        /// <summary>
+        /// Hvornår motercyklen kørte over broen
+        /// </summary>
+        public DateTime Dato { get; set; }
+
+        /// <summary>
+        /// Prisen for at krydse broen
+        /// </summary>
+        /// <returns></returns>
+        public decimal Pris() => 125;
+        /// <summary>
+        /// Retunere køretøjets type
+        /// </summary>
+        /// <returns></returns>
+        public string Køretøj() => "MC";
+    }
+}
---
+++ BilletLibrary/UnitTestProject/UnitTestMC.cs
@@ -0,0 +1,39 @@
+﻿using BilletLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTestMC
+    {
+        [TestMethod]
+        public void Pris_ReturVærdi_Retunere125()
+        {
+            //Arrange
+            MC b = new MC();
+            decimal expected = 125;
+            decimal actual = 0;
+
+            //Act
+            actual = b.Pris();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Køretøj_ReturVærdi_RetunereMC()
+        {
+            //Arrange
+            MC b = new MC();
+            string expected = "MC";
+            string actual = "";
+
+            //Act
+            actual = b.Køretøj();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
